1aeacf87b32910d8bfc7578e878bcc1c3d477523
akshay-m2 <er.akshayrc@gmail.com>
2024-01-18 04:57:09 -0800

    Add base code for the spring boot app

---
+++ src/main/java/com/akshayram/multilingualchat/MultilingualchatApplication.java
@@ -0,0 +1,13 @@
+package com.akshayram.multilingualchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultilingualchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultilingualchatApplication.class, args);
+	}
+
+}
